3872e7fa234e2823d38d22917f5417c2bd3522b0
Herlich <Herlich@gmail.com>
2020-01-24 16:58:38 -0600

    agregados los campos a la tabla de usuarios

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -15,6 +15,82 @@ export class User extends BaseEntity {
   @Column("text")
   password: string;
 
+  @Column("text")
+  name: string;
+
+  @Column("text")
+  cellPhone: string;
+
+  @Column("text")
+  DPI: string;
+
+  @Column("text")
+  customerType: string;
+
+  @Column("text")
+  verificationLVL: string;
+
+  @Column("text")
+  address: string;
+
+  @Column("text")
+  bank: string;
+
+  @Column("text")
+  bankAccountNumber: string;
+
+  @Column("text")
+  bankAccountName: string;
+
+  @Column("text")
+  taxID: string;
+
+  @Column("text")
+  taxIDName: string;
+
+  @Column("text")
+  taxIDAddress: string;
+
+  @Column("text")
+  USDBalance: number;
+
+  @Column("text")
+  BTCBalance: number;
+
+  @Column("text")
+  BTCAddress: string;
+
+  @Column("text")
+  securityQuestion1: string;
+
+  @Column("text")
+  securityQuestion2: string;
+
+  @Column("text")
+  securityAnswer1: string;
+
+  @Column("text")
+  securityAnswer2: string;
+
+  @Column("text")
+  photoDPI: string;
+
+  @Column("text")
+  photoAddress: string;
+
+  @Column("text")
+  withdrawLimitBTC: string;
+
+  @Column("text")
+  depositLimitUSD: string;
+
+  @Column("text")
+  WithdrawalLimitUSD: string;
+
+  @Column("text")
+  status: number;
+
+
   @Column("int", { default: 0 })
   tokenVersion: number;
 }
